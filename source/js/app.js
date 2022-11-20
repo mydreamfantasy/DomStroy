35a9d86cfa3edb2cecee7bc230c5f6e91bedc594
Anastasiia <nastyara13@yandex.ru>
2022-11-20 15:01:56 +0300

    Добавлена шапка для мобильной версии

---
+++ source/js/app.js
@@ -0,0 +1,19 @@
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-navigation__toggle');
+
+const toggleMenu = () => {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+}
+
+const switchMenu = () => {
+  navMain.classList.remove('main-navigation--nojs');
+  navToggle.addEventListener('click', toggleMenu);
+}
+
+switchMenu();
